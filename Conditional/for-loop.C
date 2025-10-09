c00f6facf1fc2e6acfe63e244fb756522caff690
Taffytheking06 <davidtmusk06@gmail.com>
2025-10-09 10:46:16 +0530

    for-loop

---
+++ Conditional/for-loop.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float salary, total = 0;
+    int month;
+
+    for (month = 1; month <= 5; month++){
+        printf("Enter salry for month %d: ", month);
+        scanf("%f", &salary);
+        total += salary;
+    }
+    printf("Total salary paid in 5 months = Rs.%.2f\n", total);
+    return 0;
+}
